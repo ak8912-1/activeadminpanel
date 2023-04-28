6c83fb190cfb4e6579ffd8e8796c0894f2ef0402
ak8912-1 <akhilchovatiya1118912@gmail.com>
2023-04-28 17:11:59 +0530

    added data in seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,32 @@
 #     User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: '123456', password_confirmation: '123456', role: "user") if Rails.env.development?
 # end
 
-User.create!(first_name: "Akhil", last_name: "Chovatiya", email: "akhil@example.com", password: '123456', password_confirmation: '123456', role: "admin") if Rails.env.development?
+# User.create!(first_name: "Akhil", last_name: "Chovatiya", email: "akhil@example.com", password: '123456', password_confirmation: '123456', role: "admin") if Rails.env.development?
+
+
+# User.create!(first_name: "Prayag", last_name: "Bhimani", email: "prayag@example.com", password: "123456", password_confirmation: "123456", role: "user")
+# User.create!(first_name: "Devang", last_name: "Dangar", email: "devang@example.com", password: "123456", password_confirmation: "123456", role: "user")
+# User.create!(first_name: "Suraj", last_name: "Parmar", email: "suraj@example.com", password: "123456", password_confirmation: "123456", role: "user")
+# User.create!(first_name: "Shivraj", last_name: "Chavda", email: "shivraj@example.com", password: "123456", password_confirmation: "123456", role: "user")
+# User.create!(first_name: "Yash", last_name: "Rathod", email: "yash@example.com", password: "123456", password_confirmation: "123456", role: "user")
+# User.create!(first_name: "Rohan", last_name: "Dodiya", email: "rohan@example.com", password: "123456", password_confirmation: "123456", role: "user")
+# User.create!(first_name: "Ritesh", last_name: "Doshi", email: "ritesh@example.com", password: "123456", password_confirmation: "123456", role: "user")
+# User.create!(first_name: "Dharmik", last_name: "Dudhagara", email: "dharmik@example.com", password: "123456", password_confirmation: "123456", role: "user")
+# User.create!(first_name: "Manav", last_name: "Kasundra", email: "manav@example.com", password: "123456", password_confirmation: "123456", role: "user")
+# User.create!(first_name: "Abhay", last_name: "Unagar", email: "abhay@example.com", password: "123456", password_confirmation: "123456", role: "user")
+# User.create!(first_name: "Dharmesh", last_name: "Chavda", email: "dharmesh@example.com", password: "123456", password_confirmation: "123456", role: "user")
+# User.create!(first_name: "Shrey", last_name: "Kanani", email: "shrey@example.com", password: "123456", password_confirmation: "123456", role: "user")
+# User.create!(first_name: "Meet", last_name: "Vasoya", email: "meet@example.com", password: "123456", password_confirmation: "123456", role: "user")
+
+
+# Asset.create!(name: "Mouse")
+# Asset.create!(name: "Key-Board")
+# Asset.create!(name: "Monitor")
+# Asset.create!(name: "Headphones")
+# Asset.create!(name: "Speaker")
+
+# Skill.create!(name: "Ruby")
+# Skill.create!(name: "Rails")
+# Skill.create!(name: "Python")
+# Skill.create!(name: "C++")
+# Skill.create!(name: "c#")
